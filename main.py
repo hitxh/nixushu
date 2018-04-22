15a99b0929c8a1a100ff5da4c2b9c526b307d7b2
hitxh <271002152@qq.com>
2018-04-22 12:54:47 +0800

    1

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ while True:
         num_arr = [int(v) for v in num_list]
         nixushu = 0
         for i in range(N-1):
-            sum([1 for v in num_arr[i::] if num_arr[i]>v])
-            for v in num_arr[i+1::]:
-                if num_arr[i]>v:
-                    nixushu += 1
+            nixushu += sum([1 for v in num_arr[i+1::] if num_arr[i]>v])
+            # for v in num_arr[i+1::]:
+            #     if num_arr[i]>v:
+            #         nixushu += 1
         print(nixushu)
         #break
     except:
